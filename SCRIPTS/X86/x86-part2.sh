7c62abdf3d9b9c82c3cef4396053dd622271816d
sandy91218 <52410912+sandy91218@users.noreply.github.com>
2022-09-26 19:23:12 +0800

    Update x86-part2.sh

--- SCRIPTS/X86/x86-part2.sh
+++ SCRIPTS/X86/x86-part2.sh
@@ -11,6 +11,8 @@
 #
 
 # Add applications
+# alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
 # Passwall
 git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall_package
 git clone -b luci --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall
@@ -26,6 +28,6 @@ rm -rf ./package/lean/luci-app-openclash
 # git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
 git clone -b master --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 # Modify default Hostname
 sed -i 's/OpenWrt/OtakuWrt/g' package/base-files/files/bin/config_generate
